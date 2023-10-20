1e05e5302503813adfb73bb4a103c3acff5f8c2a
quinzonjosh <quinzonjosh261@gmail.com>
2023-10-21 01:45:34 +0800

    renamed files

--- app/src/main/java/com/mobdeve/s15/nadela/oliva/quinzon/umbrellasalleapp/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/nadela/oliva/quinzon/umbrellasalleapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.mobdeve.s15.nadela.oliva.quinzon.umbrellasalleapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,22 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.forgot_password)
-
-        val forgotPasswordProceedBtn = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btn_forgotPasswordProceed)
-        forgotPasswordProceedBtn.setOnClickListener{
-//            val Intent = Intent(this.EmailVerificationSentActivity::class.java)
-        }
-
+        setContentView(R.layout.activity_main)
     }
-    /**
-     *
-     *
-     * Changes from irah-dev-red-section branch
-     *
-     *
-     * */
-
-
-
 }
