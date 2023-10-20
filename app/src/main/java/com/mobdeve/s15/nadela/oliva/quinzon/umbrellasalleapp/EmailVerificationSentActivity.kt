6c92356ee2504f8337c5c5e0037eb115f44cd4e9
quinzonjosh <quinzonjosh261@gmail.com>
2023-10-21 01:07:08 +0800

    refined layouts

---
+++ app/src/main/java/com/mobdeve/s15/nadela/oliva/quinzon/umbrellasalleapp/EmailVerificationSentActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s15.nadela.oliva.quinzon.umbrellasalleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailVerificationSentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.email_verification_sent)
+    }
+}
--- app/src/main/java/com/mobdeve/s15/nadela/oliva/quinzon/umbrellasalleapp/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/nadela/oliva/quinzon/umbrellasalleapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.mobdeve.s15.nadela.oliva.quinzon.umbrellasalleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.forgot_password)
+
+        val forgotPasswordProceedBtn = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btn_forgotPasswordProceed)
+        forgotPasswordProceedBtn.setOnClickListener{
+//            val Intent = Intent(this.EmailVerificationSentActivity::class.java)
+        }
+
     }
     /**
      *
