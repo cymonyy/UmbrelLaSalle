009f1029b2cd85d95f5131caaad3f31121a65be2
cymonyy <cymon_nadela@dlsu.edu.ph>
2023-10-22 02:47:59 +0800

    added back button config
    component back_button.xml added
    added onclicklistener

--- app/src/main/java/com/mobdeve/s15/nadela/oliva/quinzon/umbrellasalleapp/StartScreenActivity.kt
+++ app/src/main/java/com/mobdeve/s15/nadela/oliva/quinzon/umbrellasalleapp/StartScreenActivity.kt
@@ -43,6 +43,12 @@ class StartScreenActivity : AppCompatActivity() {
             }
         })
 
+        this.viewBinding.cvBackButton.root.setOnClickListener(View.OnClickListener {
+            finish()
+        })
+
+
+
 
     }
 
